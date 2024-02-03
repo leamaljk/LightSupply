e263bc8acf9b355a031c04bce5e9f31f8198e48b
Leamaljk <lea.zizic@hotmail.com>
2024-02-03 12:17:44 +0000

    tablet nav

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -49,10 +49,7 @@
   <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286m1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94"/>
 </svg>      <p class="small-nav-text">FAQ</p>
     </a>
-    <!-- <a href="#main-div" class="nav-icon">
-      <font-awesome-icon :icon="['fas', 'image']" />    
-      <p class="small-nav-text">Gallery</p>
-    </a> -->
+    
     <a href="#footer" class="nav-icon">
       <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
   <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
@@ -94,42 +91,7 @@
   font-weight: 700;
 }
 
-@media (min-width: 1026px) {
 
-  .nav-item{
-    align-items: center;
-  }
-  .nav-links, .my-icon-style {
-padding: 2px;
-
-
-}
-.my-icon-style {
-padding-right: 10px;
-margin-top: 10px;
-
-}
-  .navbar{
-    display: flex;
-    }
-  }
-
-
-  
-  @media only screen and (min-width: 767px) and (max-width: 1025px) {
-
-    .my-icon-style {
-margin: 12px;
-padding-right: 10px;
-
-}
-.navbar {
-  width: 100%;
-padding: 80px 0;
-padding-top: 20px;
-  }
-
-  }
 
 /* NAVIGATION */
 .navbar {
@@ -196,6 +158,60 @@ display: none;
 .nav-item {
     font-size: clamp(0.7rem, 2vw, 1rem); /* Adjust values as needed */
   }
+
+  @media (min-width: 767px) and (max-width: 1400px) {
+  .nav-links {
+    display: none;
+  }
+  .mobile-navbar {
+    position: absolute; /* Fix navbar to the bottom */
+  top: 0;
+  right: 0;
+  right: 0;
+  display: flex; /* Use flexbox for layout */
+  justify-content: space-around; /* Space icons evenly */
+  align-items: center; /* Center icons vertically */
+  background-color: var(--bg-color); /* Set background color */
+  padding: 30px 0; /* Add padding */
+  z-index: 1000;
+  width: 55%;
+  }
+
+
+  .nav-icon {
+  color: var(--border); /* Icon color */
+  font-size: 22px; /* Icon size */
+  display: block; /* Ensure it's a block element */
+}
+
+/* You might want to add :hover and :active states for your icons */
+.nav-icon:hover {
+  color: var(--border);
+  animation: bounce 0.1s ease;
+}
+
+a{
+  text-decoration: none;
+}
+
+
+.small-nav-text{
+display: none;
+
+}
+@keyframes bounce {
+  0%, 20%, 50%, 80%, 100% {
+    transform: translateY(0); /* Start and end with no vertical movement */
+  }
+  40% {
+    transform: translateY(-10px); /* Bounce up 10px at 40% */
+  }
+  60% {
+    transform: translateY(-5px); /* Bounce up 5px at 60% */
+  }
+}
+
+}
 
 @media (max-width: 768px) {
   .navbar, .nav-wave{
