efe13ab89485e3970c6ec2a16860f45e16defc2d
Leamaljk <lea.zizic@hotmail.com>
2024-07-19 13:00:35 +0100

    some small updates

--- src/components/CostumerTestimonials.vue
+++ src/components/CostumerTestimonials.vue
@@ -13,7 +13,7 @@
           <div class="customer-details">
             <div class="external-links">
                 <div class="linkovi">
-            <a href="https://www.myjobquote.co.uk/t/light-supply-electrical-services" target="_blank" class="e-link"><font-awesome-icon class="icon" :icon="['fas', 'arrow-up-right-from-square']" />Click to see more on MyJobQuote <span>40</span>reviews </a> 
+            <a href="https://www.myjobquote.co.uk/t/light-supply-electrical-services" target="_blank" class="e-link"><font-awesome-icon class="icon" :icon="['fas', 'arrow-up-right-from-square']" />Click to see more on MyJobQuote <span>46</span>reviews </a> 
             <a href="https://www.mybuilder.com/profile/view/light_supply/feedback" target="_blank" class="e-link"><font-awesome-icon class="icon" :icon="['fas', 'arrow-up-right-from-square']" />And MyBuilder <span>28</span>reviews</a> 
                </div>
             </div>
